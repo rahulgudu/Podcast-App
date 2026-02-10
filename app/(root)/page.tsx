b0a2f3b556e9930e4910be9913323728591fd614
sushantskb <sushantsrk2001@gmail.com>
2026-02-10 16:10:43 +0530

    homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,25 @@
+import PodCastCard from "@/components/PodCastCard";
+import { podcastData } from "@/constants";
 import { Button } from "@heroui/button";
 import React from "react";
-
 
 const Index = () => {
   return (
     <div className="flex flex-col gap-9">
       <section className="flex flex-col gap-5 mt-9">
         <h1 className="text-white-1 text-20 font-bold">Trending Podcasts</h1>
-        <Button className="text-white-1 bg-orange-1">Click Me</Button>
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => (
+            <PodCastCard
+              key={podcast.id}
+              imgURL={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
       </section>
-      
     </div>
   );
 };
---
+++ components/PodCastCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+
+const PodCastCard = ({
+  imgURL,
+  title,
+  description,
+  podcastId,
+}: {
+  imgURL: string;
+  title: string;
+  description: string;
+  podcastId: number;
+}) => {
+  return (
+    <div className="cursor-pointer">
+      <figure className="flex flex-col gap-2">
+        <Image
+          src={imgURL}
+          width={174}
+          height={174}
+          alt={title}
+          className="aspect-square h-fit w-full rounded-xl 2xl:size-50"
+        />
+        <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+        <h2 className="text-12 truncate font-normal text-white-4 capitalize">
+          {description}
+        </h2>
+      </figure>
+    </div>
+  );
+};
+
+export default PodCastCard;
